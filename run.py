002e7ed8022de25b6dd69fdb069888db4aca3e42
andreschapa <andres@scanmatics.com>
2020-12-31 16:40:47 -0600

    changed logo

--- run.py
+++ run.py
@@ -1,5 +1,11 @@
 # run.py
+
+##Change this back once ready to deploy to heroku. Running locally for development purposes
+
 import os
 from project import app
 port = int(os.environ.get('PORT', 5000))
 app.run(host='0.0.0.0', port=port)
+
+#from project import app
+#app.run()
